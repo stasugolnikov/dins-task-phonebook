c690be1fb14e9e29d6aee22953f9738b2fe36ff3
Stas <ssugol@yandex.ru>
2021-05-10 01:07:04 +0300

    Initial commit

---
+++ src/test/java/com/dins/internship/task/phonebook/PhonebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dins.internship.task.phonebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
